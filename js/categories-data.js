8187510493e20d6bf9cabc94552ac139528eeb2a
NeAlfa <sharipovdevelopper@gmail.com>
2023-07-01 05:59:00 -0700

    modal search catalog

--- js/categories-data.js
+++ js/categories-data.js
@@ -1,32 +1,32 @@
 const categories = [
-  { id: 1, name: "Fruit", image: "https://picsum.photos/200/300/?category0" },
-  { id: 2, name: "Bakery", image: "https://picsum.photos/200/300/?category2" },
+  { id: 1, name: "Fruit", image: "https://www.diabetes.co.uk/wp-content/uploads/2019/01/iStock-467652436.jpg" },
+  { id: 2, name: "Bakery", image: "https://img.freepik.com/free-photo/sweet-pastry-assortment-top-view_23-2148516578.jpg?w=2000" },
   {
     id: 3,
     name: "Vegetable",
-    image: "https://picsum.photos/200/300/?category3",
+    image: "https://www.bhg.com/thmb/Mwd_YEkDbVg_fPsUDcWr3eZk9W0=/5645x0/filters:no_upscale():strip_icc()/difference-between-fruits-vegetables-01-5f92e7ec706b463287bcfb46985698f9.jpg",
   },
-  { id: 4, name: "Dairy", image: "https://picsum.photos/200/300/?category5" },
-  { id: 5, name: "Meat", image: "https://picsum.photos/200/300/?category6" },
+  { id: 4, name: "Dairy", image: "https://media.cnn.com/api/v1/images/stellar/prod/210922092746-dairy-products-stock.jpg?q=w_2500,h_1667,x_0,y_0,c_fill" },
+  { id: 5, name: "Meat", image: "https://facts.net/wp-content/uploads/2022/06/different-types-of-meat.jpg" },
   {
     id: 6,
     name: "Dessert",
-    image: "https://picsum.photos/200/300/?category7",
+    image: "https://c.ndtvimg.com/2020-04/chd4rs3g_dessert_625x300_07_April_20.jpg",
   },
   {
     id: 7,
     name: "Fast food",
-    image: "https://picsum.photos/200/300/?category12",
+    image: "https://zamin.uz/uploads/posts/2019-11/1574165899_00.jpg",
   },
   {
     id: 8,
     name: "Breakfast",
-    image: "https://picsum.photos/200/300/?category23",
+    image: "https://images.services.kitchenstories.io/PV-rl5pSp1lL_XRe7KDBW1KGan4=/3840x0/filters:quality(80)/images.kitchenstories.io/wagtailOriginalImages/R2798-photo-final-1.jpg",
   },
-  { id: 9, name: "Lunch", image: "https://picsum.photos/200/300/?category27" },
+  { id: 9, name: "Lunch", image: "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/health/wp-content/uploads/2022/08/workday_lunch_ideas.jpeg.jpg" },
   {
     id: 10,
     name: "Dinner",
-    image: "https://picsum.photos/200/300/?category31",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2OGbyHhoTy5QHkMp4uSa9wfXC4xdpUC019Q&usqp=CAU",
   },
 ];
--- js/categories.js
+++ js/categories.js
@@ -1,10 +1,52 @@
+const searchInput = document.querySelector(".search-input");
+searchInput.addEventListener("input", () => {
+  let searchInputProducts = "";
+  let a = products.filter((el) =>
+    el.name.toLowerCase().includes(searchInput.value.toLowerCase())
+  );
+  if (searchInput.value.length > 0) {
+    document.querySelector(".search__input2").style.display = "block";
+  } else {
+    document.querySelector(".search__input2").style.display = "none";
+  }
+  a.map((item) => {
+    searchInputProducts += `        
+      <div class="search__input-box">
+        <div class="search__input-card">
+        <img src="${item.images[0]}" alt="Error" />
+          <h3>${item.name}</h3>
+          <p>${item.price} ₽</p>
+        </div>
+      </div>`;
+  });
+  document.querySelector(".search__input2").innerHTML = searchInputProducts;
+});
+
+const login = document.querySelector(".nav-right-login");
+const modal = document.querySelector("#modal");
+const closeBtn = document.querySelector(".modal-close-btn");
+login.addEventListener("click", function () {
+  if (modal.style.marginTop <= "0px") {
+    modal.style.marginTop = "0px";
+    modal.style.opacity = "1";
+    modal.style.zIndex = "999";
+  }
+});
+closeBtn.addEventListener("click", function () {
+  if (modal.style.marginTop == "0px") {
+    modal.style.marginTop = "-1500px";
+    modal.style.opacity = "0";
+    modal.style.zIndex = "-999999";
+  }
+});
+
 let card = "";
 
 categories.map((item) => {
   card += `<div class="catalog__footer--card">
-  <img src="./assets/images/categories/img.png" alt="" />
+  <img src="${item.image}" alt="" />
     <a onclick="localStorage.setItem('category', '${item.name}')" href="#">${item.name}</a>
   </div>`;
   document.querySelector(".catalog__footer").innerHTML = card;
 });
-console.log(localStorage.getItem('category'));
+console.log(localStorage.getItem("category"));
